8c1d6b9089a1831a2d732a634c5aee3398ce144e
Thinkman Wang <thinkman_wang@azhuoinfo.com>
2016-06-05 20:47:54 +0800

    add new attrs

--- thinkutils/src/main/java/com/thinkman/thinkutils/view/LoginView.java
+++ thinkutils/src/main/java/com/thinkman/thinkutils/view/LoginView.java
@@ -1,6 +1,9 @@
 package com.thinkman.thinkutils.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -49,6 +52,61 @@ public class LoginView extends RelativeLayout {
         m_btnLogin = (Button) contentView.findViewById(R.id.btn_login);
         m_tvForgetPwd = (TextView) contentView.findViewById(R.id.tv_forget_pwd);
         m_tvRegister = (TextView) contentView.findViewById(R.id.tv_register);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.LoginView, 0, 0);
+//        int n = a.getIndexCount();
+//        for (int i = 0; i < n; i++) {
+//            int attr = a.getIndex(i);
+//            if (R.styleable.LoginView_loginBtnBDRes == attr) {
+//
+//            } else if (R.styleable.LoginView_accountLabel == attr) {
+//
+//            }
+//        }
+
+        try {
+            Drawable bg = a.getDrawable(R.styleable.LoginView_loginBtnBDRes);
+            if (null != bg) {
+                setLoginBtnBG(bg);
+            }
+
+            String loginBtnLabel = a.getString(R.styleable.LoginView_loginBtnLabel);
+            if (null != loginBtnLabel && false == TextUtils.isEmpty(loginBtnLabel)) {
+                setLoginBtnLabel(loginBtnLabel);
+            }
+
+            String szLabelAccount = a.getString(R.styleable.LoginView_accountLabel);
+            if (null != szLabelAccount && false == TextUtils.isEmpty(szLabelAccount)) {
+                setAccountLabel(szLabelAccount);
+            }
+
+            String accountHint = a.getString(R.styleable.LoginView_accountHint);
+            if (null != accountHint && false == TextUtils.isEmpty(accountHint)) {
+                setAccountHint(accountHint);
+            }
+
+            String passwordText = a.getString(R.styleable.LoginView_passwordText);
+            if (null != passwordText && false == TextUtils.isEmpty(passwordText)) {
+                setPasswordLabel(passwordText);
+            }
+
+            String passwordHint = a.getString(R.styleable.LoginView_passwordHint);
+            if (null != passwordHint && false == TextUtils.isEmpty(passwordHint)) {
+                setPasswordHint(passwordHint);
+            }
+
+            String forgetPwdLabel = a.getString(R.styleable.LoginView_forgetPwdLabel);
+            if (null != forgetPwdLabel && false == TextUtils.isEmpty(forgetPwdLabel)) {
+                setForgetPwdLabel(forgetPwdLabel);
+            }
+
+            String registerLabel = a.getString(R.styleable.LoginView_registerLabel);
+            if (null != registerLabel && false == TextUtils.isEmpty(registerLabel)) {
+                setRegisterLabel(registerLabel);
+            }
+        } finally {
+            a.recycle();
+        }
     }
 
     public void setOnLoginClickListener(View.OnClickListener listener) {
@@ -69,5 +127,41 @@ public class LoginView extends RelativeLayout {
 
     public String getPassword() {
         return m_etPwd.getText().toString();
+    }
+
+    public void setLoginButtonBackgroundResource(int nResId) {
+        m_btnLogin.setBackgroundResource(nResId);
+    }
+
+    public void setLoginBtnBG(Drawable drawable) {
+        m_btnLogin.setBackground(drawable);
+    }
+
+    public void setLoginBtnLabel(String szLabel) {
+        m_btnLogin.setText(szLabel);
+    }
+
+    public void setAccountLabel(String szLabel) {
+        ((TextView)contentView.findViewById(R.id.tv_id)).setText(szLabel);
+    }
+
+    public void setAccountHint(String szHint) {
+        m_etId.setHint(szHint);
+    }
+
+    public void setPasswordLabel(String szLabel) {
+        ((TextView)contentView.findViewById(R.id.tv_pwd)).setText(szLabel);
+    }
+
+    public void setPasswordHint(String szHint) {
+        m_etPwd.setHint(szHint);
+    }
+
+    public void setForgetPwdLabel(String szLabel) {
+        m_tvForgetPwd.setText(szLabel);
+    }
+
+    public void setRegisterLabel(String szLabel) {
+        m_tvRegister.setText(szLabel);
     }
 }
