418e8e8c0dba798eb6b3cba8b8d1998a753ff1eb
Thinkman Wang <thinkman_wang@azhuoinfo.com>
2016-07-07 21:28:49 +0800

    finish CommonHorizontalAttachBar

---
+++ thinkutils/src/main/java/com/thinkman/thinkutils/view/CommonHorizontalAttachBar.java
@@ -0,0 +1,101 @@
+package com.thinkman.thinkutils.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.bumptech.glide.Glide;
+import com.thinkman.thinkutils.R;
+
+/**
+ * Created by wangx on 2016/7/7.
+ */
+public class CommonHorizontalAttachBar extends HorizontalScrollView {
+
+    Context mContent = null;
+    View m_ContentView = null;
+    LinearLayout m_llContent = null;
+
+    private int m_nTextColor;
+    private float m_fLabelTextSize;
+
+    public CommonHorizontalAttachBar(Context context) {
+        super(context);
+        init(context, null, 0);
+    }
+
+    public CommonHorizontalAttachBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
+
+    public CommonHorizontalAttachBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs, defStyle);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyle) {
+        mContent = context;
+        m_ContentView = LayoutInflater.from(context).inflate(R.layout.bar_custom_horizontal_attach, this, true);
+        m_llContent = (LinearLayout) m_ContentView.findViewById(R.id.ll_content);
+
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.CommonHorizontalAttachBar, defStyle, 0);
+
+        if (a.hasValue(R.styleable.CommonHorizontalAttachBar_labelColor)) {
+            m_nTextColor = a.getInt(R.styleable.CommonHorizontalAttachBar_labelColor, getResources().getColor(R.color.text_dark_666));
+        } else {
+            m_nTextColor = getResources().getColor(R.color.text_dark_666);
+        }
+
+        if (a.hasValue(R.styleable.CommonHorizontalAttachBar_labelTextSize)) {
+            m_fLabelTextSize = a.getDimension(R.styleable.CommonHorizontalAttachBar_labelTextSize, getResources().getDimension(R.dimen.text_size_small));
+        } else {
+            m_fLabelTextSize = getResources().getDimension(R.dimen.text_size_small);
+        }
+    }
+
+    public void addItem(String szUrl, String szText) {
+        addItem(szUrl, szText
+                , (int) mContent.getResources().getDimension(R.dimen.avatar_size)
+                , (int) mContent.getResources().getDimension(R.dimen.avatar_size));
+    }
+
+    public void addItem(String szUrl, String szText, int nWidth, int nHeight) {
+        CircleImageText citItem = new CircleImageText(mContent);
+        citItem.getTextView().setTextColor(m_nTextColor);
+        citItem.getTextView().setTextSize(TypedValue.COMPLEX_UNIT_PX, m_fLabelTextSize);
+        citItem.getTextView().setText(szText);
+
+        Glide.with(mContent)
+                .load(szUrl)
+                .override(nWidth, nHeight)
+                .into(citItem.getImageView());
+
+        addItem(citItem);
+    }
+
+    public void addItem(View view) {
+        if (null == view) {
+            return;
+        }
+
+        LinearLayout.LayoutParams layoutParams
+           = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        layoutParams.setMargins((int)mContent.getResources().getDimension(R.dimen.horizontal_attach_bar_margin)
+                , 0
+                , (int)mContent.getResources().getDimension(R.dimen.horizontal_attach_bar_margin)
+                , 0);
+
+        m_llContent.addView(view, layoutParams);
+    }
+
+
+}
--- thinkutilssample/src/main/java/com/thinkman/thinkutilssample/CustomBarActivity.java
+++ thinkutilssample/src/main/java/com/thinkman/thinkutilssample/CustomBarActivity.java
@@ -7,7 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.thinkman.thinkutils.view.CommonHorizontalAttachBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CustomBarActivity extends AppCompatActivity {
+
+    @BindView(R.id.chab_attachment)
+    CommonHorizontalAttachBar m_chabAttachment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,23 @@ public class CustomBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_custom_bar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
+        init();
+    }
+
+    private void init() {
+        m_chabAttachment.addItem("https://www.baidu.com/img/bd_logo1.png", "百度0");
+        m_chabAttachment.addItem("https://www.baidu.com/img/bd_logo1.png", "百度1");
+        m_chabAttachment.addItem("https://www.baidu.com/img/bd_logo1.png", "百度2");
+        m_chabAttachment.addItem("https://www.baidu.com/img/bd_logo1.png", "百度3");
+        m_chabAttachment.addItem("https://www.baidu.com/img/bd_logo1.png", "百度4");
+
+        m_chabAttachment.addItem("https://www.baidu.com/img/bd_logo1.png", "百度5");
+        m_chabAttachment.addItem("https://www.baidu.com/img/bd_logo1.png", "百度6");
+        m_chabAttachment.addItem("https://www.baidu.com/img/bd_logo1.png", "百度7");
+        m_chabAttachment.addItem("https://www.baidu.com/img/bd_logo1.png", "百度8");
+        m_chabAttachment.addItem("https://www.baidu.com/img/bd_logo1.png", "百度9");
     }
 
 }
