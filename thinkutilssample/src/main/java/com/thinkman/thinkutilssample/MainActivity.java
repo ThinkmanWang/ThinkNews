62cd24186cefe7d0b4013d657073eb7e3a8fc9bd
Thinkman Wang <thinkman_wang@azhuoinfo.com>
2016-07-10 22:33:48 +0800

    update

--- thinkutilssample/src/main/java/com/thinkman/thinkutilssample/MainActivity.java
+++ thinkutilssample/src/main/java/com/thinkman/thinkutilssample/MainActivity.java
@@ -15,11 +15,13 @@ import butterknife.BindView;
 import butterknife.OnClick;
 import butterknife.ButterKnife;
 
+import com.bigkoo.pickerview.OptionsPickerView;
 import com.bigkoo.pickerview.TimePickerView;
 import com.thinkman.thinkutils.dialog.CommonDialogUtils;
 import com.thinkman.thinkutils.view.ImagePickerView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import in.srain.cube.views.ptr.PtrClassicFrameLayout;
@@ -46,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
     Button m_btnDateTime = null;
 
     TimePickerView pvTime = null;
+
+    @BindView(R.id.btn_area_picker)
+    Button m_btnAreaPicker = null;
+
+    OptionsPickerView pvOptions;
+    private ArrayList<String> options1Items = new ArrayList<String>();
+    private ArrayList<ArrayList<String>> options2Items = new ArrayList<ArrayList<String>>();
+    private ArrayList<ArrayList<ArrayList<String>>> options3Items = new ArrayList<ArrayList<ArrayList<String>>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,6 +112,113 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTimeSelect(Date date) {
                 m_btnDateTime.setText(getTime(date));
+            }
+        });
+
+        initAreaPicker();
+    }
+
+    public void initAreaPicker() {
+        pvOptions = new OptionsPickerView(this);
+
+        options1Items.add(new String("广东"));
+        options1Items.add(new String("湖南"));
+        options1Items.add(new String("广西"));
+
+        //选项2
+        ArrayList<String> options2Items_01=new ArrayList<String>();
+        options2Items_01.add("广州");
+        options2Items_01.add("佛山");
+        options2Items_01.add("东莞");
+        options2Items_01.add("阳江");
+        options2Items_01.add("珠海");
+        ArrayList<String> options2Items_02=new ArrayList<String>();
+        options2Items_02.add("长沙");
+        options2Items_02.add("岳阳");
+        ArrayList<String> options2Items_03=new ArrayList<String>();
+        options2Items_03.add("桂林");
+        options2Items.add(options2Items_01);
+        options2Items.add(options2Items_02);
+        options2Items.add(options2Items_03);
+
+        //选项3
+        ArrayList<ArrayList<String>> options3Items_01 = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> options3Items_02 = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> options3Items_03 = new ArrayList<ArrayList<String>>();
+        ArrayList<String> options3Items_01_01=new ArrayList<String>();
+        options3Items_01_01.add("白云");
+        options3Items_01_01.add("天河");
+        options3Items_01_01.add("海珠");
+        options3Items_01_01.add("越秀");
+        options3Items_01.add(options3Items_01_01);
+        ArrayList<String> options3Items_01_02=new ArrayList<String>();
+        options3Items_01_02.add("南海");
+        options3Items_01_02.add("高明");
+        options3Items_01_02.add("顺德");
+        options3Items_01_02.add("禅城");
+        options3Items_01.add(options3Items_01_02);
+        ArrayList<String> options3Items_01_03=new ArrayList<String>();
+        options3Items_01_03.add("其他");
+        options3Items_01_03.add("常平");
+        options3Items_01_03.add("虎门");
+        options3Items_01.add(options3Items_01_03);
+        ArrayList<String> options3Items_01_04=new ArrayList<String>();
+        options3Items_01_04.add("其他1");
+        options3Items_01_04.add("其他2");
+        options3Items_01_04.add("其他3");
+        options3Items_01.add(options3Items_01_04);
+        ArrayList<String> options3Items_01_05=new ArrayList<String>();
+        options3Items_01_05.add("其他1");
+        options3Items_01_05.add("其他2");
+        options3Items_01_05.add("其他3");
+        options3Items_01.add(options3Items_01_05);
+
+        ArrayList<String> options3Items_02_01=new ArrayList<String>();
+        options3Items_02_01.add("长沙长沙长沙长沙长沙长沙长沙长沙长沙1111111111");
+        options3Items_02_01.add("长沙2");
+        options3Items_02_01.add("长沙3");
+        options3Items_02_01.add("长沙4");
+        options3Items_02_01.add("长沙5");
+        options3Items_02_01.add("长沙6");
+        options3Items_02_01.add("长沙7");
+        options3Items_02_01.add("长沙8");
+        options3Items_02.add(options3Items_02_01);
+        ArrayList<String> options3Items_02_02=new ArrayList<String>();
+        options3Items_02_02.add("岳1");
+        options3Items_02_02.add("岳2");
+        options3Items_02_02.add("岳3");
+        options3Items_02_02.add("岳4");
+        options3Items_02_02.add("岳5");
+        options3Items_02_02.add("岳6");
+        options3Items_02_02.add("岳7");
+        options3Items_02_02.add("岳8");
+        options3Items_02_02.add("岳9");
+        options3Items_02.add(options3Items_02_02);
+        ArrayList<String> options3Items_03_01=new ArrayList<String>();
+        options3Items_03_01.add("好山水");
+        options3Items_03.add(options3Items_03_01);
+
+        options3Items.add(options3Items_01);
+        options3Items.add(options3Items_02);
+        options3Items.add(options3Items_03);
+
+        //三级联动效果
+        pvOptions.setPicker(options1Items, options2Items, options3Items, true);
+        //设置选择的三级单位
+//        pwOptions.setLabels("省", "市", "区");
+        pvOptions.setTitle("选择城市");
+        pvOptions.setCyclic(false, true, true);
+        pvOptions.setSelectOptions(1, 1, 1);
+        pvOptions.setOnoptionsSelectListener(new OptionsPickerView.OnOptionsSelectListener() {
+
+            @Override
+            public void onOptionsSelect(int options1, int option2, int options3) {
+                //返回的分别是三个级别的选中位置
+                String tx = options1Items.get(options1)
+                        + options2Items.get(options1).get(option2)
+                        + options3Items.get(options1).get(option2).get(options3);
+
+                m_btnAreaPicker.setText(tx);
             }
         });
     }
@@ -183,5 +300,10 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.btn_date_time)
     public void onDateTimeClick() {
         pvTime.show();
+    }
+
+    @OnClick(R.id.btn_area_picker)
+    public void onAreaPickerClick() {
+        pvOptions.show();
     }
 }
